8e314ee37c071157e7ff5d3030bd39e3cb61965c
Thomas Ritter <tritter@k1r3p7.42mulhouse.fr>
2025-05-20 15:06:26 +0200

    Feat: Fixed a few bugs

--- include/ai.h
+++ include/ai.h
@@ -6,7 +6,7 @@
 /*   By: tritter <tritter@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 10:52:42 by tritter           #+#    #+#             */
-/*   Updated: 2025/05/20 13:25:29 by tritter          ###   ########.fr       */
+/*   Updated: 2025/05/20 14:35:13 by tritter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,13 @@ typedef struct grid_pos
     int y; 
     int length;
 } t_grid_pos;
+
+int         is_full(char c);
+int         legal_placement(char **grid, t_grid_pos origin);
+t_grid_pos  check_optimal(Game *data);
+t_grid_pos  optimal_move_check(Game *data, t_grid_pos origin);
+t_grid_pos  left_check(Game *data, t_grid_pos origin);
+t_grid_pos  right_check(Game *data, t_grid_pos origin);
+t_grid_pos  diagonal_right_check(Game *data, t_grid_pos origin);
+t_grid_pos  diagonal_left_check(Game *data, t_grid_pos origin);
+t_grid_pos  up_check(Game *data, t_grid_pos origin);
--- src/connect_ai.c
+++ src/connect_ai.c
@@ -6,7 +6,7 @@
 /*   By: tritter <tritter@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 10:52:19 by tritter           #+#    #+#             */
-/*   Updated: 2025/05/20 14:07:48 by tritter          ###   ########.fr       */
+/*   Updated: 2025/05/20 14:53:12 by tritter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ int is_full(char c)
         return (0);
 }
 
-int legal_placement(char **grid, t_grid_pos origin)
+int legal_placement(Game *data, t_grid_pos origin)
 {
-    if (origin.x == 0)
+    if (origin.x == data->linesSize)
         return (1);
     else
-        return (isfull(grid[origin.x - 1][origin.y]));
+        return (is_full(data->grid[origin.x + 1][origin.y]));
 
 }
 
@@ -39,9 +39,9 @@ t_grid_pos check_optimal(Game *data)
     curr.x = 0;
     curr.y = 0;
     curr.length = 0;
-    while (curr.x >= data->columnSize)
+    while (curr.x < data->columnSize)
     {
-        while (curr.y >= data->linesSize)
+        while (curr.y < data->linesSize)
         {
             to_evaluate = optimal_move_check(data, curr);
             if (to_evaluate.length > optimal.length)
@@ -72,6 +72,9 @@ t_grid_pos optimal_move_check(Game *data, t_grid_pos origin)
     if (curr.length > optimal.length)
         optimal = curr;
     curr = diagonal_right_check(data, origin);
+    if (curr.length > optimal.length)
+        optimal = curr;
+    curr = up_check(data, origin);
     if (curr.length > optimal.length)
         optimal = curr;
     return (optimal);
@@ -108,9 +111,9 @@ t_grid_pos left_check(Game *data, t_grid_pos origin)
         {
             curr.length = -1;
             return (curr);
-        } 
+        }
+        i++; 
     }
-    i++;
 }
 
 t_grid_pos right_check(Game *data, t_grid_pos origin)
@@ -123,7 +126,7 @@ t_grid_pos right_check(Game *data, t_grid_pos origin)
     curr.length = 0;
     while (i < 4)
     {
-        if (curr.y >= data->linesSize)
+        if (curr.y < data->linesSize)
         {
             if (data->grid[curr.x][curr.y] == AI)
             {
@@ -145,9 +148,9 @@ t_grid_pos right_check(Game *data, t_grid_pos origin)
         {
             curr.length = -1;
             return (curr);
-        } 
+        }
+        i++;
     }
-    i++;
 }
 
 t_grid_pos diagonal_right_check(Game *data, t_grid_pos origin)
@@ -160,7 +163,7 @@ t_grid_pos diagonal_right_check(Game *data, t_grid_pos origin)
     curr.length = 0;
     while (i < 4)
     {
-        if (curr.x >= data->linesSize && curr.y >= data->columnSize)
+        if (curr.x < data->linesSize && curr.y < data->columnSize)
         {
             if (data->grid[curr.x][curr.y] == AI)
             {
@@ -184,8 +187,8 @@ t_grid_pos diagonal_right_check(Game *data, t_grid_pos origin)
             curr.length = -1;
             return (curr);
         } 
+        i++;
     }
-    i++;
 }
 
 t_grid_pos diagonal_left_check(Game *data, t_grid_pos origin)
@@ -198,7 +201,7 @@ t_grid_pos diagonal_left_check(Game *data, t_grid_pos origin)
     curr.length = 0;
     while (i < 4)
     {
-        if (curr.x >= data->linesSize && curr.y >= data->columnSize)
+        if (curr.x < data->linesSize && curr.y > 0)
         {
             if (data->grid[curr.x][curr.y] == AI)
             {
@@ -221,7 +224,44 @@ t_grid_pos diagonal_left_check(Game *data, t_grid_pos origin)
         {
             curr.length = -1;
             return (curr);
-        } 
+        }
+        i++;
     }
-    i++;
+}
+
+t_grid_pos up_check(Game *data, t_grid_pos origin)
+{
+    t_grid_pos  curr;
+    int         i;
+    
+    i = 0;
+    curr = origin;
+    curr.length = 0;
+    while (i < 4)
+    {
+        if (curr.x < data->linesSize)
+        {
+            if (data->grid[curr.x][curr.y] == AI)
+            {
+                curr.x++;
+                curr.length++;
+            }
+            else if (data->grid[curr.x][curr.y] == NEUTRAL)
+            {
+                if (legal_placement(data->grid, curr))
+                    return (curr);
+                else 
+                {
+                    curr.length = -1;
+                    return (curr);
+                }
+            }
+        }
+        else
+        {
+            curr.length = -1;
+            return (curr);
+        } 
+        i++;
+    }
 }
